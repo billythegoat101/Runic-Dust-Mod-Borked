f43c752037c9837bbf5a72e0dd966c8505f3e6c6
Francesco <nonya@busi.ness>
2012-08-20 13:49:46 -0400

    Added example mod
    Added an example mod for how to add runes and dusts.

---
+++ common/dustexample/examplerunes/DEMakeBlockFromDustLevel.java
@@ -0,0 +1,79 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dustexample.examplerunes;
+
+import net.minecraft.src.Block;
+import net.minecraft.src.EntityPlayer;
+import net.minecraft.src.World;
+import dustmod.DustEvent;
+import dustmod.EntityDust;
+import dustmod.TileEntityDust;
+
+/**
+ *
+ * @author billythegoat101
+ */
+public class DEMakeBlockFromDustLevel extends DustEvent
+{
+    
+    public DEMakeBlockFromDustLevel()
+    {
+        super();
+    }
+    
+    @Override
+    public void onInit(EntityDust e)
+    {
+        super.onInit(e);
+        
+        int blockID = 0;
+        //Since it is a solid rune, e.dustID returns the dust it is made of
+        //And depending on that dust, we select a block we want to use
+        switch(e.dustID){
+            case 1:
+                blockID = Block.wood.blockID;
+                break;
+            case 2:
+                blockID = Block.tnt.blockID;
+                break;
+            case 3:
+                blockID = Block.blockLapis.blockID;
+                break;
+            case 4:
+                blockID = Block.netherBrick.blockID;
+                break;
+        }
+        
+        World world = e.worldObj;
+        //Loop through all the block coordinates beneath those taken up by the runes.
+        for(Integer[] i:e.dustPoints){
+            int x = i[0];
+            int y = i[1] -1;
+            int z = i[2];
+            world.setBlockWithNotify(x, y, z, blockID);
+        }
+        
+        e.fade();
+    }
+    
+    @Override
+    public void onTick(EntityDust e)
+    {
+        super.onTick(e);
+        //everything is done in init(), so there is no use for this method
+    }
+    
+    @Override
+    public void onRightClick(EntityDust e, TileEntityDust ted, EntityPlayer p)
+    {
+        super.onRightClick(e, ted, p);
+    }
+    
+    @Override
+    public void onUnload(EntityDust e)
+    {
+        super.onUnload(e);
+    }
+}
---
+++ common/dustexample/examplerunes/DEMakeItRain.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dustexample.examplerunes;
+
+import net.minecraft.src.EntityPlayer;
+import net.minecraft.src.World;
+import dustmod.DustEvent;
+import dustmod.EntityDust;
+import dustmod.TileEntityDust;
+
+/**
+ *
+ * Rune of Making it Rain
+ * 
+ * Good example of:
+ * -How to use the onRightClick function
+ * -Health sacrifices
+ * 
+ * @author billythegoat101
+ */
+public class DEMakeItRain extends DustEvent
+{
+    
+    public DEMakeItRain()
+    {
+        super();
+    }
+    
+    @Override
+    public void onInit(EntityDust e)
+    {
+        super.onInit(e);
+        //This is a really lazily made event.
+    }
+    
+    @Override
+    public void onTick(EntityDust e)
+    {
+        super.onTick(e);
+    }
+    
+    @Override
+    public void onRightClick(EntityDust e, TileEntityDust ted, EntityPlayer p)
+    {
+        super.onRightClick(e, ted, p);
+        World world = e.worldObj;
+        //Takes 2 full hearts (4 halves) from the closest player
+        if(this.takeLife(e, 4, true)){
+            
+            if(world.isRaining()){
+                world.setRainStrength(0F);
+            }else{
+                world.setRainStrength((float)(Math.random()*0.8F) + 0.2F);
+            }
+        }
+    }
+
+	@Override
+    public void onUnload(EntityDust e)
+    {
+        super.onUnload(e);
+    }
+}
